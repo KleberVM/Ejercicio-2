662a96abc53a536354ca950f6198a62fc87d5e50
Saul Alejandro Peñaloza Lujan <saulalejandro0103@gmail.com>
2024-09-17 20:40:18 +0200

    Area de un triangulo Saul Peñaloza

---
+++ areaTriangulo.py
@@ -0,0 +1,5 @@
+# Hecho por Saul Alejandro Peñaloza Lujan
+def AreaTriangulo():
+    numero1 = int(input("ingrese base: "))
+    numero2 = int(input("ingrese altura: "))
+    return (numero1 * numero2)/2
