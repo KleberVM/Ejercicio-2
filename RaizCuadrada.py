ce07384ef94ea358bb4f5a874580adbe1ae9c6f4
kleber <muruchi2018@gmail.com>
2024-09-17 23:15:33 -0400

    area triangulo pòr saul

--- RaizCuadrada.py
+++
@@ -1,7 +0,0 @@
-#Realizado por Yamil Angelo Lara Balderrama
-def calcular_raiz(num):
-    # Validar que el número sea mayor o igual a cero
-    if num < 0:
-        return "No se puede calcular la raíz cuadrada de un número negativo"
-    else:
-        return math.sqrt(num)
